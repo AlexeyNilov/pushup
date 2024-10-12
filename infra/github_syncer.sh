5455cbe7a919efe328685bea2ae7606077b7cf2a
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-12 09:45:25 +0200

    update service

--- infra/github_syncer.sh
+++ infra/github_syncer.sh
@@ -9,9 +9,9 @@ REMOTE_COMMIT=$(git rev-parse origin/main)
 BASE_COMMIT=$(git merge-base HEAD origin/main)
 
 if [ "$LOCAL_COMMIT" = "$REMOTE_COMMIT" ]; then
-    echo "The local repository is up to date with the remote repository."
+    echo "The local repository is up to date with the remote repository"
 elif [ "$LOCAL_COMMIT" = "$BASE_COMMIT" ]; then
-    echo "The local repository is behind the remote repository."
-    echo "Starting update:"
+    echo "The local repository is behind the remote repository"
+    echo "Starting update"
     git pull
     systemctl restart cody_bot.service
