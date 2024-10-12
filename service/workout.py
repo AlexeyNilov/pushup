0e5bc7117b604bc7255c9f027e40232ef5e1fbef
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-12 23:31:44 +0200

    spiking

--- service/workout.py
+++ service/workout.py
@@ -7,7 +7,7 @@ WORKOUTS = load_yaml("db/workout.yaml")
 
 def get_workout(avg_rep: int = 5, max_rep: int = 10) -> str:
     key = random.choice(list(WORKOUTS.keys()))
-    desc: str = WORKOUTS[key]["desc"]
+    desc: str = WORKOUTS[key]
     desc = desc.replace("{start_rep}", str(int(avg_rep / 2)))
     desc = desc.replace("{avg_rep}", str(avg_rep))
     desc = desc.replace("{max_rep}", str(max_rep))
--- test/test_workout.py
+++ test/test_workout.py
@@ -5,3 +5,4 @@ def test_get_workout():
     assert isinstance(get_workout(), str)
     assert "{" not in get_workout()
     assert "}" not in get_workout()
+    # assert get_workout() == ""
