7785fba8d3e0df5a75da383864ee83b9bee05d51
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-13 15:54:33 +0200

    add workout

--- service/workout.py
+++ service/workout.py
@@ -10,5 +10,7 @@ def get_workout(avg_rep: int = 5, max_rep: int = 10) -> str:
     desc: str = WORKOUTS[key]
     desc = desc.replace("{start_rep}", str(int(avg_rep / 2)))
     desc = desc.replace("{avg_rep}", str(avg_rep))
+    desc = desc.replace("{avg_rep+1}", str(avg_rep + 1))
+    desc = desc.replace("{avg_rep+2}", str(avg_rep + 2))
     desc = desc.replace("{max_rep}", str(max_rep))
     return f"<i>{key}</i>\n{desc}"
