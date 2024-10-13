a5f40b3d08fbe2627c6aab2c56303343c9aa4039
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-13 18:10:30 +0200

    spiking

--- sample/view_db.py
+++ sample/view_db.py
@@ -1,4 +1,4 @@
-from data.fastlite_db import DB, prepare_db, create_profile_table
+from data.fastlite_db import DB, prepare_db
 from fastcore.utils import hl_md
 
 
@@ -10,10 +10,6 @@ print(hl_md(t.schema, "sql"))
 for e in DB.t.event():
     print(e)
     break
-
-t = DB.t.profile
-t.drop()
-create_profile_table()
 
 t = DB.t.profile
 print(hl_md(t.schema, "sql"))
